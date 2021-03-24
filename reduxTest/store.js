a9a151adbba279fb1ffe2241baa269bcedf64dc6
cheonseungmin <dudghsx12@gmail.com>
2021-03-24 18:11:42 +0900

    set redux-devtools

--- reduxTest/store.js
+++ reduxTest/store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux'
+import { createStore } from 'redux';
 import BReducer from './BReducer.js'
 
-const store = createStore(BReducer)
+const store = createStore(
+    BReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 
 export default store
